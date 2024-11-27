3ab697041f2d5c35248e9bd2351e070d7cd43704
Randy Johnson <Randall.Johnson@nih.gov>
2024-11-27 16:18:59 -0500

    Add `op`

--- tests/testthat/test-gemini_query.R
+++ tests/testthat/test-gemini_query.R
@@ -1,27 +1,7 @@
 # test `gemini_query` function
 
-# command to pull Gemini API key from 1Password (see https://developer.1password.com/docs/cli/get-started/)
-
-# this script will fail if `op` isn't installed or if the correct entry is missing from 1Password
-
-# Windows: if op is installed and works on the powershell but isn't working here, set the OP environmental variable:
-# run `(Get-Command op).Source` in the power shell (i.e. in a terminal window)
-# copy the path returned (ending with "\" and excluding "op.exe")
-# open .Renviron with `usethis::edit_r_environ()` and add a new line with the something like the following (making sure you put "\\" for each backslash):
-# OP=C:\\path\\to\\op\\ 
-# (if OP isn't defined, `Sys.getenv("OP")` will return "", so it should work either way)
-cmnd <- "op read op://Private/Gemini/api_key"
-
-if(Sys.info()[['sysname']] == "Windows")
-{
-  key <- system(paste0(Sys.getenv("OP"), cmnd), intern = TRUE)
-  key <- system(paste0('powershell -Command "', cmnd, '"'), intern = TRUE)
-    #shell(cmnd, shell = "powershell", intern = TRUE)
-}else{
-  key <- system(cmnd, intern = TRUE)
-}
-
-Sys.setenv(GEMINI_API_KEY = key)
+# this will need to be updated to test this on a system other than the author's
+Sys.setenv(GEMINI_API_KEY = op("Private", "Gemini", "api_key"))
 
 test_that("query works",
 {
