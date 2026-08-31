9b471eee63ccc7c7de11b9609e0483acab4b6d55
Randy Johnson <johnson@hood.edu>
2026-08-31 09:55:37 -0400

    Expand encryption to character vectors

--- R/encryption.R
+++ R/encryption.R
@@ -5,19 +5,20 @@
 # Sys.setenv(ENCRYPTION_KEY = sodium::bin2hex(key))
 
 #' encrypt_text
-#' Encrypt text using the sodium library
+#' Encryption and decryption of text using the sodium library
 #' 
-#' @param plain_text Character string to encrypt
+#' @param plain_text Character vector to encrypt
 #' @param key_hex Encryption key
 #' 
 #' @details You will need to generate a 32-byte key (or load one, e.g. from `.Renviron`). See examples below.
+#' 
 #' @examples
 #' # set ENCRYPTION_KEY
 #' key <- sodium::random(32)
 #' Sys.setenv(ENCRYPTION_KEY = sodium::bin2hex(key))
 #' 
 #' # encrypt
-#' (encrypted <- encrypt_text("Sensitive text to encrypt"))
+#' (encrypted <- encrypt_text(c("Sensitive text to encrypt", "abc123")))
 #' 
 #' # decrypt
 #' decrypt_text(encrypted)
@@ -25,8 +26,24 @@
 #' @return Encrypted text
 #' @export
 #' @importFrom OPsecrets get_secret
-#' @importFrom sodium data_encrypt hex2bin random
 encrypt_text <- function(plain_text, key_hex = get_secret("ENCRYPTION_KEY", "Private", "learnr", "key_hex"))
+{
+  encrypted_text <- character(length(plain_text))
+
+  for(i in 1:length(plain_text))
+  {
+    encrypted_text[i] <- encrypt_text_one(plain_text[i])
+  }
+
+  return(encrypted_text)
+}
+
+#' encrypt_text_one
+#' @rdname encrypt_text
+#' 
+#' @importFrom OPsecrets get_secret
+#' @importFrom sodium bin2hex data_encrypt hex2bin random
+encrypt_text_one <- function(plain_text, key_hex = get_secret("ENCRYPTION_KEY", "Private", "learnr", "key_hex"))
 {
   if (is.na(plain_text) || is.null(plain_text)) return(NA_character_)
 
@@ -51,6 +68,23 @@ encrypt_text <- function(plain_text, key_hex = get_secret("ENCRYPTION_KEY", "Pri
 #' @importFrom OPsecrets get_secret
 #' @importFrom sodium data_decrypt hex2bin
 decrypt_text <- function(hex_cipher, key_hex = get_secret("ENCRYPTION_KEY", "Private", "learnr", "key_hex"))
+{
+  plain_text <- character(length(hex_cipher))
+
+  for(i in 1:length(hex_cipher))
+  {
+    plain_text[i] <- decrypt_text_one(hex_cipher[i])
+  }
+
+  return(plain_text)
+}
+
+#' decrypt_text_one
+#' @rdname encrypt_text
+#' 
+#' @importFrom OPsecrets get_secret
+#' @importFrom sodium data_decrypt hex2bin
+decrypt_text_one <- function(hex_cipher, key_hex = get_secret("ENCRYPTION_KEY", "Private", "learnr", "key_hex"))
 {
   if (is.na(hex_cipher) || is.null(hex_cipher)) return(NA_character_)
 
